66d89e3596d9faaa96c35e5e524adc87efd902f7
anusharao13 <anurao9015@gmail.com>
2020-03-27 14:56:52 +0530

    Commiting first time

---
+++ Test Suites/Rest-API-testing.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Rest-API-testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>029b39f5-ade6-4204-a0ea-4088ee28f397</testSuiteGuid>
+   <testCaseLink>
+      <guid>71a575f8-86b8-40ea-9451-854933e59db5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/REST-Service/ListUser</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>39163e61-9399-4744-97e7-607e7aae519f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/REST-Service/TestHttpMethods</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
